68bc14fc02a3541a02a17b4e7be2576e5a8db828
Kosate Limpongsa <kosatelim@gmail.com>
2018-12-16 12:22:39 +0700

    Auto format

--- functions/binbuddy.py
+++ functions/binbuddy.py
@@ -4,6 +4,7 @@ import os
 
 from metadata import bin_type_to_name_dict, name_to_bin_dict
 
+
 class BinBuddy:
 
     def __init__(self, config=None):
@@ -12,22 +13,23 @@ class BinBuddy:
         else:
             self.config = config
 
-        self.client = boto3.client('rekognition', aws_access_key_id=self.config['rekognition']['aws_access_key_id'], aws_secret_access_key=self.config['rekognition']['aws_secret_access_key'], region_name=self.config['rekognition']['region_name'])
-    
+        self.client = boto3.client('rekognition', aws_access_key_id=self.config['rekognition']['aws_access_key_id'],
+                                   aws_secret_access_key=self.config['rekognition']['aws_secret_access_key'], region_name=self.config['rekognition']['region_name'])
+
     def detect_label_from_path(self, image_path):
         with open(image_path, 'rb') as image:
             response = self.client.detect_labels(Image={'Bytes': image.read()})
 
-        print('Detected labels in ' + image_path)    
+        print('Detected labels in ' + image_path)
         for label in response['Labels']:
-            print (label['Name'] + ' : ' + str(label['Confidence']))
+            print(label['Name'] + ' : ' + str(label['Confidence']))
 
         return response['Labels']
 
     def detect_label_from_byte(self, image_byte):
         response = self.client.detect_labels(Image={'Bytes': image_byte})
         for label in response['Labels']:
-            print (label['Name'] + ' : ' + str(label['Confidence']))
+            print(label['Name'] + ' : ' + str(label['Confidence']))
 
         return response['Labels']
 
@@ -52,17 +54,17 @@ class BinBuddy:
             except:
                 bin_type = 0
                 bin_type_text = bin_type_to_name_dict[bin_type]
-                
-            # if bin_type > 0:
-            text = 'ตรวจพบ "{}" เป็นขยะประเภท "{}"'.format(detected_object, bin_type_text)
-            output_texts.append(text)
+
+            if bin_type > 0:
+                text = 'ตรวจพบ "{}" เป็นขยะประเภท "{}"'.format(
+                    detected_object, bin_type_text)
+                output_texts.append(text)
 
         return "\n".join(output_texts)
+
 
 if __name__ == "__main__":
     binbuddy = BinBuddy(config=_config)
     with open('../test-image/test.jpg', 'rb') as image:
         labels = binbuddy.which_bin_to_thrash(image.read())
     print(labels)
-
-
--- functions/main.py
+++ functions/main.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import (MessageEvent, TextMessage, ImageMessage, TextSendMessage)
+from linebot.models import (
+    MessageEvent, TextMessage, ImageMessage, TextSendMessage)
 from binbuddy import BinBuddy
 
 load_dotenv()
@@ -11,43 +12,51 @@ line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_TOKEN'))
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 image_path = os.path.join(os.getcwd(), "image.jpg")
 binbuddy = BinBuddy(config={
-  "rekognition": {
-    "aws_access_key_id": os.getenv('AWS_ACCESS_KEY_ID'),
-    "aws_secret_access_key": os.getenv('AWS_SECRET_ACCESS_KEY'),
-    "region_name": os.getenv('REGION_NAME')
-  }
+    "rekognition": {
+        "aws_access_key_id": os.getenv('AWS_ACCESS_KEY_ID'),
+        "aws_secret_access_key": os.getenv('AWS_SECRET_ACCESS_KEY'),
+        "region_name": os.getenv('REGION_NAME')
+    }
 })
+
 
 def LineBot(request):
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
 
     try:
-      handler.handle(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
-      abort(400)
-    
+        abort(400)
+
     return 'OK'
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-  line_bot_api.reply_message(
-    event.reply_token,
-    TextSendMessage(text=binbuddy.which_bin_to_thrash_word(event.message.text))
-  )
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(
+            text=binbuddy.which_bin_to_thrash_word(event.message.text))
+    )
+
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
-  message_content = line_bot_api.get_message_content(event.message.id)
+    # line_bot_api.reply_message(
+    #   event.reply_token,
+    #   TextSendMessage(text="น้องไม่สามารถทำหน้าที่ต่อได้แล้ว ฮบือ")
+    # )
+    message_content = line_bot_api.get_message_content(event.message.id)
 
-  image = bytearray()
-  for chunk in message_content.iter_content():
-    image.extend(chunk)
-  
-  labels = binbuddy.which_bin_to_thrash_image(image)
-  if labels == "":
-    labels = "ไม่พบข้อมูลประเภทขยะในฐานข้อมูล"
-  line_bot_api.reply_message(
-    event.reply_token,
-    TextSendMessage(text=labels)
-  )
+    image = bytearray()
+    for chunk in message_content.iter_content():
+        image.extend(chunk)
+
+    labels = binbuddy.which_bin_to_thrash_image(image)
+    if labels == "":
+        labels = "ไม่พบข้อมูลประเภทขยะในฐานข้อมูล"
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=labels)
+    )
